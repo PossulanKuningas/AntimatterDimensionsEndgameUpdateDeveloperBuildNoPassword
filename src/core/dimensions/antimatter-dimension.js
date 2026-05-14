43fe8668434636166c8cb84f7d61a34c709d797a
Supersonic Seven <b.rockholz@icloud.com>
2026-05-14 17:41:54 -0400

    Update conditions for production calculations

--- src/core/dimensions/antimatter-dimension.js
+++ src/core/dimensions/antimatter-dimension.js
@@ -649,10 +649,10 @@ class AntimatterDimensionState extends DimensionState {
         const log10 = production.log10().log10();
         production = Decimal.pow10(Decimal.pow10(Decimal.pow(log10, DivinityUpgrade.divineL1U4.effectOrDefault(1) * Accelerators.cosmic.effectValue3)));
       }
-      if (ResurgenceUpgrade.ipSurge.isBought) {
+      if (ResurgenceUpgrade.ipSurge.isBought && !player.disablePostReality) {
         production = production.times(gainedInfinityPoints());
       }
-      if (ResurgenceUpgrade.epSurge.isBought) {
+      if (ResurgenceUpgrade.epSurge.isBought && !player.disablePostReality) {
         production = production.times(gainedEternityPoints());
       }
       if (production.gt(Decimal.pow10(1e200)) && !Pelle.isDoomed) {
